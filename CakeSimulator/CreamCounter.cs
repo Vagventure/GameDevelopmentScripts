d2073e031706760ff8f1073a47415ccbbab3e0e6
Vagventure <ankitverma0366@gmail.com>
2026-04-19 19:25:19 +0530

    Bug fixes to creamCounter and microwave

--- CakeSimulator/CreamCounter.cs
+++ CakeSimulator/CreamCounter.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class CreamCounter : BaseCounter
 {
-    
+    [SerializeField] private KitchenObjectsSO icingSO;
+    public override void Interact(Player player)
+    {
+
+        if (player.HasKitchenObject())
+        {
+           
+            //Add icing to the base
+            if (player.GetKitchenObjects().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                plateKitchenObject.TryAddIngridient(icingSO);  
+            }
+
+        }
+        
+    }
 }
+
